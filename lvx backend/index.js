fa496bf4aaf0b7c2cf7defbe2fd864b55f331657
NihitYadav <nihityadav8@gmail.com>
2023-04-25 11:38:13 +0530

    backend updation

---
+++ lvx backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const itemRouter = require("./routes/itemRoutes");
+const userRouter = require("./routes/userRoutes");
+
+const app = express();
+
+//MIDDLEWARES
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+//ROUTES
+app.use("/api/v1/items", itemRouter);
+app.use("/api/v1/users", userRouter);
+
+module.exports = app;
---
+++ lvx backend/models/itemModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+  seller: {
+    type: String,
+    required: [true, "Seller must have a name"],
+  },
+  itemname: {
+    type: String,
+    required: [true, "Item must have a name"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Item must have a price"],
+  },
+  whatsapp: Number,
+  lpuid: {
+    type: Number,
+    required: [true, "Seller must have a lpu id"],
+  },
+  description: String,
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+module.exports = Item;
